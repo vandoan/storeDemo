e49c2746baef9cc77441ee750af8702b52fcfe08
Van Doan <vdoan09@gmail.com>
2014-07-03 13:35:16 -0400

    created a basic layout for a store and some tests.

---
+++ test/models/product_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  test "product attributes must nob be empty" do
+    product= Product.new 
+    assert product.invalid? 
+    assert product.errors[:title].any? 
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+   end
+ 
+
+
+test "product price must be positive" do 
+	product = Product.new(title: "My Book Title", description: "yyy", image_url: "zzz.jpg")
+	product.price = -1
+	assert_equal ["must be greater than or equal to 0.01" ], product.errors[:price]
+
+	product.price = 0
+	assert_equal ["must be greater than or equal to 0.01" ]
+		product.errors[:price]
+
+	product.price = 1 
+	assert product.valid? 
+	end 
+
+end 
